a7beb28b5a5b9858c28b6abec6ce8605267bb7b4
Jakub Karbowski <karbo360@gmail.com>
2022-08-22 16:28:23 +0200

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+
+function navToggle(){
+    btn.classList.toggle('open');
+    overlay.classList.toggle('overlay-show')
+    document.body.classList.toggle('stop-scrolling');
+    menu.classList.toggle('show-menu');
+}
+
+function reset(){
+    counters.forEach((counter) => (counter.innerHTML = '0'))
+}
+
+function scrollPage(){
+    const scrollPos = window.scrollY;
+    if(scrollPos > 100 && !scrollStarted){
+        countUp();
+        scrollStarted = true;
+    }else if(scrollPos < 100&& scrollStarted){
+        reset();
+        scrollStarted= false;
+    }
+}
+
+function countUp(){
+counters.forEach((counter) => {
+    counter.innerText ='0';
+
+    const updateCounter = () =>{
+const target = +counter.getAttribute('data-target');
+const c = +counter.innerText;
+
+const increment = target /100;
+if(c <target){
+counter.innerText =`${Math.ceil(c + increment)}`;
+
+setTimeout(updateCounter, 75);
+}else{
+    counters.innerText =target;
+}
+
+    }
+    updateCounter();
+} );
+}
+
+btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage)
+
+
+countUp();
+
